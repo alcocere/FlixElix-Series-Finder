28730050d17616f579b84c851f7a017d4ace23d3
alcocere <elisa.alcocerm@gmail.com>
2021-02-08 19:34:10 +0100

    Add fetch API

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
+//HTML elements
+const searchInput = document.querySelector('.js-search');
 
-console.log('>> Ready :)');
+// Array que se llenara una vez que la api regrese los datos buscados
+let series = [];
+
+//API TVMaze
+function getDataFromApi() {
+    fetch(`http://api.tvmaze.com/search/shows?q=${'show'}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            series = data;
+        });
+}
+getDataFromApi();
+
