62f4c1cb1cf61f2bbc333aefc3c30c6c27d5edcb
alcocere <elisa.alcocerm@gmail.com>
2021-02-08 21:47:42 +0100

    Add render shows list

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,41 @@
+/* eslint-disable indent */
 'use strict';
+
 //HTML elements
 const searchInput = document.querySelector('.js-search');
+const showsResult = document.querySelector('.js-tvshows');
 
 // Array que se llenara una vez que la api regrese los datos buscados
 let series = [];
 
 //API TVMaze
 function getDataFromApi() {
-    fetch(`http://api.tvmaze.com/search/shows?q=${'show'}`)
+    fetch(`http://api.tvmaze.com/search/shows?q=${'girls'}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             series = data;
+
+            renderShows();
         });
 }
+
+function renderShows() {
+    let htmlCode = '';
+    for (let i = 0; i < series.length; i++) {
+
+        htmlCode += `<li class="js-show">`;
+        htmlCode += `<h5 class="js-show-name">${series[i].show.name}</h5>`;
+        htmlCode += `<img src="" ${series[i].show.name}" class="js-show-img/>`;
+        htmlCode += `</li>`;
+    }
+    showsResult.innerHTML = htmlCode;
+}
+
+function handleSearch() {
+}
+
+searchInput.addEventListener('keyup', handleSearch);
+
 getDataFromApi();
 
