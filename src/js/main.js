7968901c15f1e1c80a6c0ecc329880fa9a2edc4c
alcocere <elisa.alcocerm@gmail.com>
2021-02-09 15:47:42 +0100

    Add styles for series results

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,13 @@ const formElement = document.querySelector('.js-form');
 const searchInput = document.querySelector('.js-search');
 const btnElement = document.querySelector('.js-btn');
 const showsResult = document.querySelector('.js-tvshows');
+const favElements = document.querySelector('.js-list-favorites');
 
 // Array que se llenara una vez que la api regrese los datos buscados
 let series = [];
 let favSeries = [];
 
-//SEARCH API TVMaze
+//SEARCH API TVMaze -------------------------------------------------------------------
 const getDataFromApi = () => {
     const urlApi = '//api.tvmaze.com/search/shows?q=';
     const userSearch = searchInput.value;
@@ -27,7 +28,7 @@ function handleSearch() {
     getDataFromApi();
 }
 
-//RENDER
+//RENDER SHOWS ------------------------------------------------------------------------
 function renderShows() {
     let htmlCode = '';
 
@@ -36,12 +37,13 @@ function renderShows() {
         let idSerie = series[i].show.id;
         htmlCode += `<li class="js-show" id="${idSerie}">`;
         htmlCode += '<div class="js__show--container">';
-        htmlCode += `<h5 class="js__show--name">${series[i].show.name}</h5>`;
+
         if (imgURL === null) {
-            htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" title="${series[i].show.name}" alt="${series[i].show.name} cover not available"/>`;
+            htmlCode += `<img class="js__show--img"src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" title="${series[i].show.name}" alt="${series[i].show.name} cover not available"/>`;
         } else {
             htmlCode += `<img src="${imgURL.medium}" alt="Show image" ${series[i].show.name}" class="js-show-img"></img>`;
         }
+        htmlCode += `<h5 class="js__show--name">${series[i].show.name}</h5>`;
         htmlCode += '</div>';
         htmlCode += `</li>`;
     }
@@ -53,15 +55,31 @@ function handleForm(ev) {
     ev.preventDefault();
 }
 
-//FAVS
+//FAVS --------------------------------------------------------------------------------
+
 const handleFavs = (ev) => {
     const selectedSerie = parseInt(ev.currentTarget.id);
-    let clickedObjet = series.find((item) => selectedSerie === item.show.id);
+    // console.log(event.currentTarget.id);
+    //consigue el nombre y id de se series donde se hace click
+    let clickedShow = series.find((item) => selectedSerie === item.show.id);
     let isFavorite = favSeries.findIndex((item) => selectedSerie === item.show.id);
+    //revisa si el item ya existe en el array de favoritos
     if (isFavorite === -1) {
-        favSeries.push(clickedObjet);
-    } else { favSeries.splice(isFavorite, 1) }
-    console.log(favSeries);
+        //agrega al array el item seleccionado
+        favSeries.push(clickedShow);
+    } else {
+        //quita el item del array
+        favSeries.splice(isFavorite, 1);
+    }
+    //cambiar color de fondo serie seleccionada
+    if (selectedSerie.classList.contains('favorite__item--list')) {
+        selectedSerie.classList.add('favorite__item--list');
+    } else {
+        selectedSerie.classList.remove('favorite__item--list');
+    }
+    favElements.innerHTML = '';
+
+    listenFavs();
 };
 
 
@@ -72,9 +90,5 @@ function listenFavs() {
     }
 }
 
-
 formElement.addEventListener('submit', handleForm);
 btnElement.addEventListener('click', handleSearch);
-
-
-
